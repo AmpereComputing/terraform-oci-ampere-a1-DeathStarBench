cf243e0958e2ae439a9d22b6fa8da645253549a5
Peter Pouliot <peter@pouliot.net>
2023-03-01 11:31:29 -0500

    updating schema and adding outputs

--- output.tf
+++ output.tf
@@ -1,14 +1,21 @@
 # Output the private and public IPs of the instance
 
-output "Ampere_DeathStarBench_PrivateIPs" {
+output "AmpereA1_DeathStarBench_PrivateIPs" {
   value = ["${oci_core_instance.ampere_a1.*.private_ip}"]
 }
 
-output "Ampere_DeathStarBench_PublicIPs" {
+output "AmpereA1_DeathStarBench_PublicIPs" {
   value = ["${oci_core_instance.ampere_a1.*.public_ip}"]
 }
 
+output "AmpereA1_DeathStarBench_URL" {
+  value = ["http://${oci_core_instance.ampere_a1.0.public_ip}:32080"]
+}
+output "AmpereA1_DeathStarBench_RESULTS_URL" {
+  value = ["http://${oci_core_instance.ampere_a1.1.public_ip}"]
+}
+
 # Output the boot volume IDs of the instance
-output "Ampere_DeathStarBench_BootVolumeIDs" {
+output "AmpereA1_DeathStarBench_BootVolumeIDs" {
   value = ["${oci_core_instance.ampere_a1.*.boot_volume_id}"]
 }
