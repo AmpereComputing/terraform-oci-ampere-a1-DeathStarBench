4006daa75ace4d7bc8043e8723cc9cb30f03d8f3
Peter Pouliot <peter@pouliot.net>
2023-10-12 12:59:01 -0400

    fixing template to templatefile drift

--- populate_dsb_backend.tf
+++ populate_dsb_backend.tf
@@ -1,14 +1,6 @@
-data "template_file" "dsb_populate" {
-  template = file("${path.module}/templates/dsb_populate.sh.tpl")
-  vars = {
-    dsb_private_ip = oci_core_instance.ampere_a1.0.private_ip
-  }
-}
-
 resource "null_resource" "populate_dsb_backend" {
   triggers = {
     instance_public_ip = oci_core_instance.ampere_a1.0.public_ip
-    template_content = data.template_file.dsb_populate.rendered
   }
   connection {
     type        = "ssh"
@@ -18,7 +10,7 @@ resource "null_resource" "populate_dsb_backend" {
   }
 
   provisioner "file" {
-    content = data.template_file.dsb_populate.rendered
+    content = templatefile("${path.module}/templates/dsb_populate.sh.tpl", { dsb_private_ip = oci_core_instance.ampere_a1.0.private_ip })
     destination = "/home/ubuntu/dsb_populate.sh"
   }
 
@@ -35,22 +27,10 @@ resource "null_resource" "populate_dsb_backend" {
     ]
   }
 }
-data "template_file" "run_wrk" {
-  template = file("${path.module}/templates/run_wrk.sh.tpl")
-  vars = {
-    dsb_private_ip = oci_core_instance.ampere_a1.0.private_ip
-    w_threads = var.wrk_number_of_threads
-    w_conns = var.wrk_number_of_conns
-    w_duration = var.wrk_duration
-    w_rps = var.wrk_requests_per_second
-  }
-}
-
 
 resource "null_resource" "execute_wrk" {
   triggers = {
     instance_public_ip = oci_core_instance.ampere_a1.1.public_ip
-    template_content = data.template_file.dsb_populate.rendered
   }
   depends_on = [ null_resource.populate_dsb_backend ]
   connection {
@@ -61,7 +41,7 @@ resource "null_resource" "execute_wrk" {
   }
 
   provisioner "file" {
-    content = data.template_file.run_wrk.rendered
+    content = templatefile("${path.module}/templates/run_wrk.sh.tpl", { dsb_private_ip = oci_core_instance.ampere_a1.0.private_ip,  w_threads = var.wrk_number_of_threads,  w_conns = var.wrk_number_of_conns,  w_duration = var.wrk_duration,  w_rps = var.wrk_requests_per_second })
     destination = "/home/ubuntu/run_wrk.sh"
   }
 
